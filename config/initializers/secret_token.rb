3b6664c25b870d806bf8fc60cd3f4fb34b0c1234
threekings <3thkings@gmail.com>
2011-06-14 14:10:50 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamesSite::Application.config.secret_token = '848e664d1d02facf991efc95b83bb6e9843cde2f6227c083a598883edfb738811b9bd261e9b0fe8a0403cf7d63ccf8156da6e0eedb2d17a167b117276491aad9'
